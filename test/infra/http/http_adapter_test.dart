7bfa1156bc61cc65de2f4b4307a36f27f47a1b70
Gabriel Mendes <gabrielmendes96@hotmail.com>
2022-11-18 17:06:19 -0300

    Ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,10 +20,13 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
+
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
     await client.post(
       Uri.parse(url),
       headers: headers,
-      body: jsonEncode(body),
+      body: jsonBody,
     );
   }
 }
@@ -59,6 +62,20 @@ void main() {
             'accept': 'application/json',
           },
           body: '{"any_key":"any_value"}',
+        ),
+      );
+    });
+
+    test('Shoud call post without body', () async {
+      await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      verify(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
         ),
       );
     });
