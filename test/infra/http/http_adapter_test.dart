164383aff3bcc87fd71db8da04e4d22a0c44af1b
Gabriel Mendes <gabrielmendes96@hotmail.com>
2022-11-21 21:50:16 -0300

    Ensure HttpAdaptor throws UnauthorizedError on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -119,6 +119,14 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test('Shoud return UnauthorizedError if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
     test('Shoud return ServerError if post returns 500', () async {
       mockResponse(500);
 
